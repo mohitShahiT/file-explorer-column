832672ec1b7d9b32787b41c3154b2f1f65f4a184
Mohit Shahi <moheetshahi92@gmail.com>
2025-08-07 22:06:14 +0545

    chore: add api server for production

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:8000";
 export const FOLDERS_KEY = "folders";
